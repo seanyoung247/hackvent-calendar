fc4869419277b1d3fc51dbaadcccedb8a806547f
Sean Young <seanyoung247@gmail.com>
2022-11-10 11:53:53 +0000

    refactor: management reordering

--- manage.py
+++ manage.py
@@ -1,47 +1,49 @@
+
 import os
 import sys
 
 
-def show_help():
-    print('I heard you need help!')
+class add_module:
+    def help(self):
+        return "Usage: "
 
-def ensure_args(count):
-    def decorator(func):
-
-        def wrapper(*args, **kwargs):
-            if len(*args) >= count:
-                func(*args, **kwargs)
-            else:
-                print('ARRRGH! not enough args!')
-
-        return wrapper
-    return decorator
+    def __call__(self, args):
+        """ Generates a new server module and files in the required format """
+        if (len(args) < 2):
+            print(self.help())
+            return
+        
+        print(args)
 
 
-@ensure_args(count = 2)
-def add_module(args):
-    """ Generates a new server module and files in the required format """
-
-    print(f"Creating module: {args[1]}")
-    # More here...
-
-
-def decode_args(args):
-    commands = {
-        "addmodule": add_module,
+def commands():
+    return {
+        "addmodule": add_module(),
         "help": show_help,
         "?": show_help,
     }
-    return commands.get(args[0])
+
+
+def show_help(args = None):
+    print('I heard you need help!')
+
+
+def decode_args(args):
+    return commands().get(args[0])
 
 
 def main(args):
     command = decode_args(args)
     if command:
+        command.help()
         command(args)
     else:
         print('Command line argument not recognised!')
-        
+
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    if len(sys.argv) > 1: 
+        main(sys.argv[1:])
+    else: 
+        # Print help here...
+        pass
