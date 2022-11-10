e2c5d718b32a2a1a870c8b585e4ea068d0777d2b
Sean Young <seanyoung247@gmail.com>
2022-11-10 17:28:47 +0000

    feat: Adds help text to manage.py

--- manage.py
+++ manage.py
@@ -1,49 +1,85 @@
 
-import os
+""" Simplifies basic management operations of the modular flask server """
+
+# import os
 import sys
 
 
-class add_module:
-    def help(self):
-        return "Usage: "
+class Command:
+    """ Base class for management commands """
+    @staticmethod
+    def help():
+        """ returns a string of help text for using this command """
+        return None
+
+    @classmethod
+    def valid_args(cls, args, count):
+        """ Checks that the correct number of arguments has been provided """
+        if len(args) >= count:
+            return True
+        print(cls.help())
+        return False
+
+    def __call__(self, args):
+        """ Ensures the correct number of arguments are passed """
+
+
+class AddModule(Command):
+    """ Adds a new server module """
+    @staticmethod
+    def help():
+        return 'Usage: python manage.py addmodule <module name>'
 
     def __call__(self, args):
         """ Generates a new server module and files in the required format """
-        if (len(args) < 2):
-            print(self.help())
+        if not self.valid_args(args, 2):
             return
-        
+        # add module code here
         print(args)
 
 
+class ShowHelp(Command):
+    """ Shows all commands help text """
+    @staticmethod
+    def help():
+        return 'Shows help text'
+
+    def __call__(self, args=None):
+        print('Usage: python manage.py <command> <options>')
+        print('Commands:')
+        commandlist = commands()
+        for (key,value) in commandlist.items():
+            print(f"{key:>15} : {value.help()}")
+
+
+
 def commands():
+    """ Enumerates all recognised commands """
     return {
-        "addmodule": add_module(),
-        "help": show_help,
-        "?": show_help,
+        'addmodule': AddModule(),
+        'help': ShowHelp(),
+        '?': ShowHelp(),
     }
 
 
-def show_help(args = None):
-    print('I heard you need help!')
-
-
 def decode_args(args):
-    return commands().get(args[0])
+    """ Returns which command object implements the requested command """
+    command = commands().get(args[0])
+    if command:
+        return command
+    return commands().get('help')
 
 
 def main(args):
+    """ Entry point """
+    if len(args) <= 1:
+        commands().get('help')()
+        return
+
+    args = args[1:]
     command = decode_args(args)
-    if command:
-        command.help()
-        command(args)
-    else:
-        print('Command line argument not recognised!')
+    command(args)
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1: 
-        main(sys.argv[1:])
-    else: 
-        # Print help here...
-        pass
+    main(sys.argv)
